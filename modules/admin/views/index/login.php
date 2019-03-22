3956fe6d4a941fee48d031b6c097e157058baf13
yehua <1104753531@qq.com>
2019-03-22 18:43:41 +0800

    aa

--- modules/admin/views/index/login.php
+++ modules/admin/views/index/login.php
@@ -7,15 +7,15 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="/modules/admin/assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/admin/assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="/modules/admin/assets/bower_components/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/admin/assets/bower_components/font-awesome/css/font-awesome.min.css">
     <!-- Ionicons -->
-    <link rel="stylesheet" href="/modules/admin/assets/bower_components/Ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="/admin/assets/bower_components/Ionicons/css/ionicons.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="/modules/admin/assets/dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="/admin/assets/dist/css/AdminLTE.min.css">
     <!-- iCheck -->
-    <link rel="stylesheet" href="/modules/admin/assets/bower_components/iCheck/square/blue.css">
+    <link rel="stylesheet" href="/admin/assets/bower_components/iCheck/square/blue.css">
 
 </head>
 <body class="hold-transition login-page">
@@ -65,9 +65,9 @@
 <!-- /.login-box -->
 
 <!-- jQuery 3 -->
-<script src="/modules/admin/assets/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="/admin/assets/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="/modules/admin/assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="/admin/assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <script>
     $(function () {
         $("#submit").click(function () {

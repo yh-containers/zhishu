c1e799075eb8bd7c336bd3a4a7b3988d1d255fe9
yehua <1104753531@qq.com>
2019-03-29 16:39:40 +0800

    Update index.php

--- views/index/index.php
+++ views/index/index.php
@@ -9,7 +9,7 @@ $this->params = [
 <?php $this->beginBlock('content')?>
 <header class="header index_header">
     <div class="content">
-        <div class="left"><i class="icon iconfont icon-yuanbao-copy"></i>元宝数：<span id="user-money"><?=$user_info['money']?></span></div>
+        <div class="left"><i class="icon iconfont icon-yuanbao-copy"></i><span id="user-money"><?=$user_info['money']?></span></div>
         <div class="center"><a href="<?=\yii\helpers\Url::to(['mine/recharge'])?>"><i class="icon iconfont icon-chongzhi1"></i><span>充值</span></a></div>
         <div class="right"><a href="<?=\yii\helpers\Url::to(['mine/withdraw'])?>"><i class="icon iconfont icon-tixian1"></i><span>提现</span></a></div>
     </div>

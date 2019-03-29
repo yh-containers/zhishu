a2a0851515d150db6d961073b59c2fbdded42803
yehua <1104753531@qq.com>
2019-03-29 11:01:31 +0800

    add

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -282,7 +282,9 @@ class IndexController extends CommonController
                 $current_model->up_time=$next_model['time'];
                 $current_model->up_price=$next_model['current_price'];//当前价格
                 $current_model->compare=$current_model['current_price']>$current_model['up_price']?2:($current_model['current_price']<$current_model['up_price']?1:3);//价格比较1涨 2跌 3平
-                $current_model->save();
+                $state=$current_model->save();
+                var_dump($state);
+                var_dump($current_model->getAttributes());
             }
 
         }
